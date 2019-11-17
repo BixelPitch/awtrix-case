e06cfa6219b446224ababca1ca0e8d0d5503757b
BixelPitch <hannes@bixelspace.de>
2019-11-17 22:24:24 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # awtrix-case
-A bunch of svg files meant to be used in laserweb for making your own wooden chassis for awtrix.
+> The current status of the project is **work in progress** - No prototype was built, no precise measurement was done. It is still in the beginning, but I keep you updated. Check out the roadmap!
+
+## Motivation
+Once upon a time I found this cool project called [Awtrix](https://docs.blueforcer.de/#/v2/README) by [blueforcer](https://github.com/Blueforcer), fell in love with it instantly, started buying parts and solder them together. The first plan was to 3D-print the chassis like it's mentioned in the documentation, but then the slicing program told me the biggest piece takes 20 hours to print... A guy in the makerspace I am building this thing said to me "Why do you want to waste mineral oil in plastic form if you can build your box with wood? We have a laser cutter btw." And I thought why not!
+
+## The plan
+The plan is pretty simple: Cut multiple layers of 4mm wooden sheets, stack and combine them with glue, nuts and bolts. In the end you should have a nice clean wooden front and back and marbled sides because of the edges of the wooden pieces. The last step would be sandpaper and a nice varnish to finish things off - Everybody can print things!
+
+The images/schematics can be found in the directory [/svg](/svg). 
+
+## Roadmap
+- ~~📝 Draw/generate the svg files~~
+- 🎛 Cut the first parts and tweak the parameters if needed
+- 🛠 Build the first prototype
+- 🖼 Upload fotos of the make
+- 📔 Create a nice documentation
---
+++ index.js
@@ -0,0 +1,124 @@
+const window = require('svgdom');
+const fs = require('fs');
+
+const { document } = window;
+const {
+  SVG, registerWindow, Rect, G, Circle,
+} = require('@svgdotjs/svg.js');
+
+registerWindow(window, document);
+
+// ++++++++++++++++++++++
+// + Configurations     +
+// ++++++++++++++++++++++
+
+// 1mm = 1
+const STROKE = 0.5;
+
+const CASE_WIDTH = 350;
+const CASE_HEIGHT = 110;
+const CASE_ROUNDNESS = 10;
+
+const LED_CARVING = 9.5;
+const LED_MARGIN = 0.5;
+
+const HOLE_MARGIN = 10;
+const HOLE_RADIUS = 2.5;
+
+// ++++++++++++++++++++++
+// + Parts              +
+// ++++++++++++++++++++++
+
+const base = new Rect()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .attr({
+    rx: CASE_ROUNDNESS,
+    yx: CASE_ROUNDNESS,
+  })
+  .fill('none')
+  .stroke({ color: 'black', width: STROKE });
+
+const holes = new G()
+// left top
+  .add(new Circle()
+    .radius(HOLE_RADIUS)
+    .fill('none')
+    .stroke({ color: 'black', width: STROKE })
+    .center(HOLE_MARGIN, HOLE_MARGIN))
+// right top
+  .add(new Circle()
+    .radius(HOLE_RADIUS)
+    .fill('none')
+    .stroke({ color: 'black', width: STROKE })
+    .center(CASE_WIDTH - HOLE_MARGIN, HOLE_MARGIN))
+// left bottom
+  .add(new Circle()
+    .radius(HOLE_RADIUS)
+    .fill('none')
+    .stroke({ color: 'black', width: STROKE })
+    .center(HOLE_MARGIN, CASE_HEIGHT - HOLE_MARGIN))
+// right bottom
+  .add(new Circle()
+    .radius(HOLE_RADIUS)
+    .fill('none')
+    .stroke({ color: 'black', width: STROKE })
+    .center(CASE_WIDTH - HOLE_MARGIN, CASE_HEIGHT - HOLE_MARGIN));
+
+const leds = new G();
+for (let x = 0; x < 32; x += 1) {
+  for (let y = 0; y < 8; y += 1) {
+    new Rect()
+      .size(LED_CARVING, LED_CARVING)
+      .move(x * LED_CARVING + x * LED_MARGIN, y * LED_CARVING + y * LED_MARGIN)
+      .fill('none')
+      .stroke({ color: 'black', width: STROKE })
+      .addTo(leds);
+  }
+}
+
+const innerCarving = new Rect()
+  .size(32 * LED_CARVING + 31 * LED_MARGIN, 8 * LED_CARVING + 7 * LED_MARGIN)
+  .fill('none')
+  .stroke({ color: 'black', width: STROKE });
+
+// ++++++++++++++++++++++
+// + Files              +
+// ++++++++++++++++++++++
+
+if (!fs.existsSync('svg')) fs.mkdirSync('svg');
+
+// frontplate
+fs.writeFileSync('svg/frontplate.svg', SVG()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .add(base)
+  .add(leds.center(CASE_WIDTH / 2, CASE_HEIGHT / 2))
+  .svg());
+
+// frontplate with holes
+fs.writeFileSync('svg/frontplate_holes.svg', SVG()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .add(base)
+  .add(holes)
+  .add(leds.center(CASE_WIDTH / 2, CASE_HEIGHT / 2))
+  .svg());
+
+// backplate
+fs.writeFileSync('svg/backplate.svg', SVG()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .add(base)
+  .svg());
+
+// backplate with holes
+fs.writeFileSync('svg/backplate_holes.svg', SVG()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .add(base)
+  .add(holes)
+  .svg());
+
+// middleplate
+fs.writeFileSync('svg/middleplate.svg', SVG()
+  .size(CASE_WIDTH, CASE_HEIGHT)
+  .add(base)
+  .add(holes)
+  .add(innerCarving.center(CASE_WIDTH / 2, CASE_HEIGHT / 2))
+  .svg());
